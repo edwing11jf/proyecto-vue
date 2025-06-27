ff61bfd10739a653bee30249dd139448295d6c11
edwingjf <edwing_alexis_11@hotmail.com>
2025-06-27 12:02:44 -0600

    Avance vid 5

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,4 +1,6 @@
 <template>
+  
+<MainView></MainView>
 <Contador></Contador>
 <ListaDeTareas></ListaDeTareas>
 </template>
@@ -6,6 +8,7 @@
 <script setup>
 import Contador from './components/Contador.vue';
 import ListaDeTareas from './components/ListaDeTareas.vue';
+import MainView from './views/MainView.vue'
 </script>
 
 <style scoped>
---
+++ vue-project/src/components/SideBar.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="sidebar">
+<h2>Barra Lateral</h2>
+<ul>
+    
+</ul>
+<<router-link to="/contador">Contador</router-link>
+<<router-link to="/lista-de-tareas">Lista de Tareas</router-link> 
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.sidebar{
+    width: 200px;
+    background-color: #b4afaf;
+    padding: 20px;
+}
+
+</style>
